8fb606f437d7108523fea7893f001f3e249e2de4
Andrewchik <andriy.goryachiy16@gmail.com>
2021-12-23 17:32:44 +0200

    updated contract name

--- src/Constants/wax.constants.js
+++ src/Constants/wax.constants.js
@@ -12,7 +12,7 @@ export const WAX_RPC_ENDPOINTS_PROTOCOL = "https";
 //export const ATOMIC_ASSETS_API = 'https://wax.api.atomicassets.io/atomicassets/v1'; //MAINNET
 export const ATOMIC_ASSETS_API = 'https://test.wax.api.atomicassets.io/atomicassets/v1'; //TESTNET
 
-export const RTP_GAME = 'rtp';
+export const RTP_GAME = 'rush2prosper';
 export const RTP_GAME_COLLECTION = 'rtp';
 export const EOSIO_TOKEN ='eosio.token';
 
