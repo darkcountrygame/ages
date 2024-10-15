52a5e4c3ffd2c2ce2d0138c195c94fba050efe18
Andrewchik <andriy.goryachiy16@gmail.com>
2024-10-15 19:53:19 +0300

    [DELETE] Swap menu and Upgrade

--- src/components/FooterGameNav/FooterGameNavLinks.js
+++ src/components/FooterGameNav/FooterGameNavLinks.js
@@ -1,8 +1,8 @@
 import workplacesIcon from '../../images/nav-icon/workplaces.png'
 import inventoryIcon from '../../images/nav-icon/inventory.png'
-import upgradeIcon from '../../images/nav-icon/upgrade.png'
+// import upgradeIcon from '../../images/nav-icon/upgrade.png'
 import researchIcon from '../../images/nav-icon/research.png'
-import swapIcon from '../../images/nav-icon/swap.png'
+// import swapIcon from '../../images/nav-icon/swap.png'
 
 
 
@@ -17,16 +17,16 @@ export const navMenu = [
         icon: inventoryIcon,
         url: '/inventory'
     },
-    {
-        title: 'Upgrade',
-        icon: upgradeIcon,
-        url: '/upgrade'
-    },
-    {
-        title: 'Swap',
-        icon: swapIcon,
-        url: '/swap'
-    },
+    // {
+    //     title: 'Upgrade',
+    //     icon: upgradeIcon,
+    //     url: '/upgrade'
+    // },
+    // {
+    //     title: 'Swap',
+    //     icon: swapIcon,
+    //     url: '/swap'
+    // },
     {
         title: 'Craft',
         icon: researchIcon,
